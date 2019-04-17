52f0f8945570c70c96208da175b5fd0b357292b1
github username <metelskyi88@gmail.com>
2019-04-17 17:23:22 -0400

    Revert "popconfirm on delete"
    This reverts commit fb29ac8bd758f2525a9f976b926141cb60da7581.

--- src/components/GemItem.js
+++ src/components/GemItem.js
@@ -12,17 +12,17 @@ const GemItem = (props) => {
   console.log(props.song);
   return(
     <React.Fragment>
-      <List.Item
-        actions={[<Button size={'small'} shape={"circle"} icon={"play-circle"}
-        onClick={() =>
-          {props.playingTrackFromGemItem(props.token, props.song);
-          props.startPlayback(props.playbackStatus);
-          setTimeout(() => props.fetchingCurrentSong(props.token), 1000)}
-      }></Button>,
-      <Button size={'small'} shape={"circle"} onClick={() => props.removerSongFromPile(props.song)}> <i class="fas fa-heart-broken"></i></Button>]}>
-        <List.Item.Meta
-          title={props.song.artist}
-          description={props.song.name}/ >
+      <List.Item>
+        <strong>{props.song.artist}</strong> - {props.song.name}
+        &nbsp;
+        <Button size={'small'} shape={"circle"} icon={"play-circle"}
+          onClick={() =>
+            {props.playingTrackFromGemItem(props.token, props.song);
+            props.startPlayback(props.playbackStatus);
+            setTimeout(() => props.fetchingCurrentSong(props.token), 1000)}
+        }></Button>
+        &nbsp;&nbsp;
+        <Button size={'small'} shape={"circle"} onClick={() => props.removerSongFromPile(props.song)}> <i class="fas fa-heart-broken"></i></Button>
       </List.Item>
     </React.Fragment>
   )
--- src/containers/HiddenGem.js
+++ src/containers/HiddenGem.js
@@ -10,34 +10,34 @@ const {Title, Text} =  Typography
 
 
 class HiddenGem extends React.Component {
+  confirm(e) {
+    console.log(e);
+    message.success('Click on Yes');
+  }
 
   cancel(e) {
     console.log(e);
-    message.error('Save it for later!');
+    message.error('Click on No');
   }
-
   render(){
     return(
       <React.Fragment>
         <div id={'hiddenGem'}>
         <Row type="flex" justify="space-around" align="middle">
-          <Col span={8}></Col>
-          <Col span={8}><Title level={3}>{this.props.hiddenGem? this.props.hiddenGem.name : console.log('no name')}
-            <Popconfirm title="Do you want to scrap you pile?" onConfirm={() => {this.props.deleteHiddenGem(this.props.hiddenGem); message.success('You have scraped your pile')}} onCancel={this.cancel} okText="Yes" cancelText="No">
-              <Button id={'deleteButton'} type={'circle'} size={'small'}icon={'delete'}></Button>
+          <Col span={10}></Col>
+          <Col span={4}><Title level={3}>{this.props.hiddenGem? this.props.hiddenGem.name : console.log('no name')}
+            <Popconfirm title="Are you sure delete this task?" onConfirm={this.confirm} onCancel={this.cancel} okText="Yes" cancelText="No">
+              <Button id={'deleteButton'} type={'circle'} size={'small'}icon={'delete'} onClick={() => {this.props.deleteHiddenGem(this.props.hiddenGem)}}></Button>
             </Popconfirm>
             </Title>
           </Col>
-        <Col span={8}></Col>
-          </Row>
-          <Row>
           <Col span={8}></Col>
-          <Col span={8}>
-            <List>
-              {this.props.gemSongs.length > 0 ? this.props.gemSongs.map(song => <GemItem key={song.id} song={song}/>) : <Text strong> You do not have any gems:( </Text>}
-            </List></Col>
-            <Col span={8}></Col>
-            </Row>
+          <Col span={4}></Col>
+          <Col span={12}><List>
+            {this.props.gemSongs.length > 0 ? this.props.gemSongs.map(song => <GemItem key={song.id} song={song}/>) : <Text strong> You do not have any gems:( </Text>}
+          </List>
+          </Col>
+          </Row>
         </div>
       </React.Fragment>
     )
