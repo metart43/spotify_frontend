8b329239c96b2fe191c3c29478b5a883dd5b7cf2
github username <metelskyi88@gmail.com>
2019-04-17 20:35:20 -0400

    basic styling

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import {Layout, Typography, Row} from 'antd'
+import {NavLink} from 'react-router-dom'
 
-const {Text} = Typography
+const {Text, Title} = Typography
 
 const WelcomePage = (props) => {
+  console.log(props.user);
   return(
       <Layout style={{height: '100vh', background: 'white'}}>
-    {props.user?  <Row>
-      <Text>Hey {props.user.name}! Go and have fun with my app!</Text>
+    {props.user?
+      <Row type='flex' justify='center' align='middle' id={'welcomePage'}>
+      <Text>Hey <Text strong>{props.user.display_name}</Text>!
+        <br/>
+        Welcome to my Hidden Gem Finder!
+        <br/>
+        This application will help you to discover brand new music and listen to new unknown bands.
+        <br/>
+        All playlist are currated by me and and some of my other friends who are musicians.
+        <br/>
+        Feel free to make your own <Text mark>Pile</Text> and store all the Hidden Gems that you find here!
+        <br/>
+        <NavLink to='/playlists'>Take me there!</NavLink>
+      </Text>
       </Row> :
-
-      `To use this application you must be signed in with a valid Spotify Account`}
+      <Row type='flex' justify='center' align='middle' id={'welcomePage'}>
+        Please, press on Sign In Button to start using the application.
+        <br/>
+        Keep in mind that you will need authorize my application to perform certain actions.
+    </Row>
+     }
     </Layout>
   )
 }
